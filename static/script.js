c5ce9db5ad942bf03105cef9ecd9eac569d892fa
asmaa-data <elshawkey544@gmail.com>
2026-05-31 18:13:18 +0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,11 @@
+// SHOW ALERT ON ACTIONS
+
+function showMessage(msg) {
+    alert(msg);
+}
+
+// CONFIRM DELETE
+
+function confirmDelete(path) {
+    return confirm("Are you sure you want to delete: " + path);
+}
